142776f85f1100e3c7aea44923bc8de26c2827e1
centek134 <centek134@wp.pl>
2025-03-12 14:38:09 +0100

    Changes If you click on post it will be displayed

--- src/app/(protected)/post/[id].tsx
+++ src/app/(protected)/post/[id].tsx
@@ -2,14 +2,18 @@ import { View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import posts from "../../../../assets/data/posts.json";
+import PostListItem from "../../../components/PostListItem";
+
 
 export default function DetailedPost() {
     const {id} = useLocalSearchParams();
+    const detailedPost = posts.find((post) => post.id === id);
 
-    const detailedPost = posts.find((post) => post.id === id)
   return (
     <View>
-      <Text>{id}</Text>
+      {detailedPost?
+      <PostListItem post={detailedPost} isDetailedPost/> :
+      <Text style={{fontSize: 24, textAlign:"center"}}>Post not found!</Text>}
     </View>
-  )
-}
+  );
+};
