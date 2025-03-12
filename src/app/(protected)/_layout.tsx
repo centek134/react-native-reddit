7ad1f8782f842728f8cc2132fd1f8d5944ac81cf
centek134 <centek134@wp.pl>
2025-03-12 14:38:57 +0100

    changes
    Added stylings for header in post/[id].tsx

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,5 +1,7 @@
-import { Redirect, Slot, Stack } from "expo-router"
-import { useAuth } from "@clerk/clerk-expo"
+import { Redirect, router, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+import { AntDesign, MaterialIcons, Entypo } from "@expo/vector-icons"
+import { View } from "react-native";
 
 export default function AppLayout() {
   const { isSignedIn } = useAuth()
@@ -11,6 +13,21 @@ export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="post/[id]" 
+      options={{
+        headerTitle: "",
+        headerStyle: { backgroundColor: "#FF5700"},
+        animation: "slide_from_right",
+        headerBackButtonDisplayMode: "minimal",
+        headerLeft: () => <AntDesign name="back" size={24} color="#fff" onPress={() => router.back()}/>,
+        headerRight: () => 
+          <View style={{ flexDirection: "row", gap: 10}}>
+            <AntDesign name="search1" size={24} color="#fff"/>
+            <MaterialIcons name="sort" size={27} color="#fff"/>
+            <Entypo name="dots-three-horizontal" size={24} color="#fff"/>
+          </View>
+        }}/>
     </Stack>
   )
 };
